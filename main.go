88e8983dd5d3a85df182c7bbccf9c45a8bf5b6ac
seafooler <seafooler@gmail.com>
2023-03-05 21:03:07 -0600

    Wait time specified by the argument of WaitTime

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ func main() {
 	}
 
 	// wait for each node to start
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second * time.Duration(conf.WaitTime))
 
 	if err = node.EstablishP2PConns(); err != nil {
 		panic(err)
