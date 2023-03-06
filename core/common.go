8ff2c463b1ade62aaf5a6e4e7e0303f78aa5942d
seafooler <seafooler@gmail.com>
2023-03-06 03:07:40 -0600

    Debugging

--- core/common.go
+++ core/common.go
@@ -2,9 +2,9 @@ package core
 
 const HASHSIZE = 32
 
-// NewTxBatch creates a new transaction batch, consisting of transaction
+// NewTxBatch creates a new transaction batch, consisting of transaction hashes
 func NewTxBatch(txNUm int) []byte {
-	var payload = make([]byte, txNUm*512)
+	var payload = make([]byte, txNUm*HASHSIZE)
 	payload[txNUm*512-1] = 1
 	return payload
 }
