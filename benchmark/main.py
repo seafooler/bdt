f0503fccfafab9d33afac1243ca420380de45bfe
wangguanxiong1 <44332829+wangguanxiong1@users.noreply.github.com>
2023-07-28 09:46:19 +0800

    Delete benchmark directory

--- benchmark/main.py
+++
@@ -1,8 +0,0 @@
-from logs import LogParser, ParseError
-def main():
-    try:
-        print(LogParser.process('./logs').result())
-    except ParseError as e:
-        print('Failed to parse logs', e)
-if __name__ == "__main__":
-    main()
